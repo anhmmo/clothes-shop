079bc23338470bbe38058c8b4d75cf08d879a908
anhmmo <aun@metropolia.fi>
2020-04-24 17:27:22 +0300

    add Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import RouterURL from "./RouterURL/RouterURL";
+import Navigation from "./Pages/Navigation/Navigation";
 
 function App() {
   return (
     <div>
+      <Navigation />
       <RouterURL />
     </div>
   );

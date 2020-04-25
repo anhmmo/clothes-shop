31a3f8ea43f2c5d478e048412549ed3b84aec2cb
anhmmo <aun@metropolia.fi>
2020-04-25 14:37:42 +0300

    add SignUp component to Registratioon pages

--- src/Pages/Registration/Registration.js
+++ src/Pages/Registration/Registration.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Registration.scss";
 import SignIn from "../../Components/SignIn/SignIn";
+import SignUp from "../../Components/SignUp/SignUp";
 
 function Registration(props) {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 }

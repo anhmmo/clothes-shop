48da4003c284f5d25f85f4d0ff12fb3a6afe2279
anhmmo <aun@metropolia.fi>
2020-05-12 14:04:48 +0300

    create new variables when user type some thing in input field, the focusOnInput lenght will count

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./Input.scss";
 function Input({ handleChange, label, ...otherProps }) {
+  let focusOnInput = otherProps.value.length; // when user type it mean length > 0 => true
+
   return (
     <div className="group">
       <input className="form-input" onChange={handleChange} {...otherProps} />
       {label ? (
-        <label
-          className={`${
-            otherProps.value.length ? "shrink" : ""
-          } form-input-label`}
-        >
+        <label className={`${focusOnInput ? "shrink" : ""} form-input-label`}>
           {label}
         </label>
       ) : null}

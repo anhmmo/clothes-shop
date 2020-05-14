92fe5fabb1994b38761f092a690d0ca4cca43a16
anhmmo <aun@metropolia.fi>
2020-05-14 14:26:30 +0300

    import CartIcon and CartDropdown into Navbar

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase";
 import { ReactComponent as Logo } from "../../Assets/crown.svg";
 import "./Navigation.scss";
+import { connect } from "react-redux";
+import CartIcon from "../../Components/CartIcon/CartIcon";
+import CartDropdown from "../../Components/CartDropdown/CartDropdown";
 
-const Navigation = ({ currentUser }) => (
+const Navigation = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -13,7 +16,9 @@ const Navigation = ({ currentUser }) => (
       <Link className="option" to="/shop">
         SHOP
       </Link>
-
+      <Link className="option" to="/contact">
+        CONTACT
+      </Link>
       {currentUser ? (
         <div className="option" onClick={() => auth.signOut()}>
           SIGN OUT
@@ -23,11 +28,15 @@ const Navigation = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
-      <Link className="option" to="/contact">
-        CONTACT
-      </Link>
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-export default Navigation;
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
+
+export default connect(mapStateToProps)(Navigation);

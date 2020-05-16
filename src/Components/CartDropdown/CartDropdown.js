c22fc2769f63d4ca6dc73088e98c4e79742e74d0
anhmmo <aun@metropolia.fi>
2020-05-16 22:12:15 +0300

    create CartDropDown space

--- src/Components/CartDropdown/CartDropdown.js
+++ src/Components/CartDropdown/CartDropdown.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import CustomButton from "../Button/Button";
 import CartItem from "../CartItem/CartItem";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+
 import "./CartDropdown.scss";
 
 const CartDropdown = ({ cartItems }) => (
@@ -15,8 +18,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);

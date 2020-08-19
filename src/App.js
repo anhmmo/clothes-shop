eab73183eb5bbd2c33f719e230cb9407474569a0
anhmmo <aun@metropolia.fi>
2020-08-19 12:00:38 +0300

    import app.scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import RouterURL from "./RouterURL/RouterURL";
 import { connect } from "react-redux";
 import Navigation from "./Pages/Navigation/Navigation";
